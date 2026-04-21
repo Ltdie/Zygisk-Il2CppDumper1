74564292bc19052dd705b573e3b2de40d81f43d6
Ltdie <67688523+Ltdie@users.noreply.github.com>
2026-04-21 11:03:06 +0800

    Update xdl_iterate.c

--- module/src/main/cpp/xdl/xdl_iterate.c
+++ module/src/main/cpp/xdl/xdl_iterate.c
@@ -40,6 +40,12 @@
 #include "xdl_linker.h"
 #include "xdl_util.h"
 
+#define LOG_TAG "Perfare"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+
 /*
  * =========================================================================================================
  * API-LEVEL  ANDROID-VERSION  SOLUTION
