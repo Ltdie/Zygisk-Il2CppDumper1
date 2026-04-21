b0310476a90827c58348018ebfa30115d037bbbc
Ltdie <67688523+Ltdie@users.noreply.github.com>
2026-04-21 11:06:27 +0800

    Update xdl_iterate.c

--- module/src/main/cpp/xdl/xdl_iterate.c
+++ module/src/main/cpp/xdl/xdl_iterate.c
@@ -40,6 +40,11 @@
 #include "xdl_linker.h"
 #include "xdl_util.h"
 
+#ifndef ZYGISK_IL2CPPDUMPER_LOG_H
+#define ZYGISK_IL2CPPDUMPER_LOG_H
+
+#include <android/log.h>
+
 #define LOG_TAG "Perfare"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
